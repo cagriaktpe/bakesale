3ab534191332b0e6ebc7920528451de8296bd913
Samet Çağrı Aktepe <sametcagriaktepe@gmail.com>
2023-08-29 13:01:52 +0200

    Set up api environment

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,27 @@
 import React from 'react';
 import {SafeAreaView, Text, StyleSheet, View} from 'react-native';
+import ajax from '../ajax';
+import DealList from './DealList';
 
 class App extends React.Component {
+  state = {
+    deals: [],
+  };
+
+  async componentDidMount() {
+    const deals = await ajax.fetchInitialDeals();
+    this.setState({deals});
+  }
+
   render() {
     return (
       <SafeAreaView style={styles.safearea}>
         <View style={styles.container}>
-          <Text style={styles.header}>Helloo</Text>
+          {this.state.deals.length > 0 ? (
+            <DealList deals={this.state.deals} />
+          ) : (
+            <Text style={styles.header}>Bakesale</Text>
+          )}
         </View>
       </SafeAreaView>
     );
---
+++ src/components/DealList.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import {Text, View} from 'react-native';
+
+class DealList extends React.Component {
+  render() {
+    return (
+      <View>
+        <Text>Deal List</Text>
+      </View>
+    );
+  }
+}
+
+export default DealList;
