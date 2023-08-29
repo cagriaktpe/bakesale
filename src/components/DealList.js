74ef494d07a863b259e2c001fd2e69ebacace4f5
Samet Çağrı Aktepe <sametcagriaktepe@gmail.com>
2023-08-29 13:09:19 +0200

    Update DealList.js
    Display list using FlatList

--- src/components/DealList.js
+++ src/components/DealList.js
@@ -1,15 +1,30 @@
 import React from 'react';
 
-import {Text, View} from 'react-native';
+import {FlatList, StyleSheet, Text, View} from 'react-native';
+import PropTypes from 'prop-types';
 
 class DealList extends React.Component {
+  static propTypes = {
+    deals: PropTypes.array.isRequired,
+  };
   render() {
     return (
-      <View>
-        <Text>Deal List</Text>
+      <View style={styles.list}>
+        <FlatList
+          data={this.props.deals}
+          renderItem={({item}) => <Text>{item.title}</Text>}
+        />
       </View>
     );
   }
 }
+
+const styles = StyleSheet.create({
+  list: {
+    backgroundColor: '#eee',
+    flex: 1,
+    width: '100%',
+  },
+});
 
 export default DealList;
